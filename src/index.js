9821607131bec30d018ba76fe48fb863d70e7a06
Eric Elliott <support@paralleldrive.com>
2019-12-13 16:19:21 -0800

    Add fallback
    * Add fallback function
    * Fix bug in Maybe
    * Refactor Maybe
    * Fix types

--- src/index.js
+++ src/index.js
@@ -6,21 +6,32 @@ const isJust = x => exists(x) && x.length;
  * it to a maybe array.
  * A maybe array is an array of one value (just(value))
  * or zero values (Nothing).
- * @param  {Any} value
+ * @param  {*} value
  * @return {Array}
  */
-const toMaybe = value => [value].filter(exists);
+const toMaybe = (value = undefined) => [value].filter(exists);
 
-/** maybe = (b, f: a => b) => Maybe[a] => b
+/** maybe = (b, f: a => b) => Maybe(a) => b
  * Take a fallback value, a function to map over, and a maybe.
  * If the maybe contains a value, return the result of applying
  * the function to the value. Otherwise, return the fallback value.
  * This function is curried.
- * @param  {Any} fallback A default fallback
- * @param  {Function}   A function to map over the value
+ * @param  {*} fallback A default fallback
+ * @param  {Function} f A function to map over the value
  * @return {Function}   A function that takes a Maybe and returns the map result
  */
-const maybe = (fallback, f) => Maybe => Maybe.map(f)[0] || fallback;
+const maybe = (fallback, f) => Maybe =>
+  isJust(Maybe) ? f(Maybe[0]) : fallback;
+
+/** fallback = (fallbackFn: Function, f: Function) => (value: Any) => Any
+ * Apply a fallback function if `value` does not exist.
+ * Otherwise, apply `f` to `value`.
+ * @param {Function} fallbackFn A function to call if value does not exist (undefined, null)
+ * @param {*} f A function to apply to the value if the value does exist.
+ * @returns {Function}
+ */
+const fallback = (fallbackFn, f) => value =>
+  exists(value) ? f(value) : fallbackFn();
 
 /** values = (list: [...Maybe]) => [...Any]
  * Take a list of maybes and return a list of Just values, excluding Nothings.
@@ -32,5 +43,6 @@ const values = list => [].concat(...list.filter(isJust));
 module.exports = {
   toMaybe,
   maybe,
-  values
+  values,
+  fallback
 };
